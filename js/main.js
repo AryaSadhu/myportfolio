a821b5323c7679f74de499854e954a42536521ce
AryaSadhu <madearyasadhu@gmail.com>
2022-07-08 16:25:18 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.addEventListener("scroll",function(){
+const header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+learnmoreBtn.addEventListener("click", () => {
+    modal(i);
+
+});
+});
+
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+
+
+
+
+
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+imgCard.addEventListener("click", () => {
+    portfolioModal(i);
+
+});
+});
+
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
